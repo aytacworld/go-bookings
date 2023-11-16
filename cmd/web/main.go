8ca1ae4df956eaafc64eb07ee2a8c57e40fadc4d
Adem Aytac <adem@aytacworld.com>
2023-11-16 17:05:03 +0100

    log helpers added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/aytacworld/go-bookings/internal/config"
 	"github.com/aytacworld/go-bookings/internal/handlers"
+	"github.com/aytacworld/go-bookings/internal/helpers"
 	"github.com/aytacworld/go-bookings/internal/models"
 	"github.com/aytacworld/go-bookings/internal/render"
 )
@@ -18,6 +20,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // main this is the main function
 func main() {
@@ -43,6 +47,12 @@ func run() error {
     // change this to true in prod
 	app.InProduction = false
 
+    infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+    app.InfoLog = infoLog
+
+    errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+    app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -63,6 +73,7 @@ func run() error {
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
+    helpers.NewHelpers(&app)
 
     return nil
 }
