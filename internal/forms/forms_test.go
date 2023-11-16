704b709c3486f8f12b69de5c2845c8649f3c7ce3
Adem Aytac <adem@aytacworld.com>
2023-11-16 16:52:19 +0100

    testing forms

--- internal/forms/forms_test.go
+++ internal/forms/forms_test.go
@@ -41,3 +41,45 @@ func TestForm_Required(t *testing.T) {
 	}
 }
 
+func TestForm_Has(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	hasVar := form.Has("a")
+	if hasVar {
+		t.Error("form says has var a, but it doesn't")
+	}
+
+	postedData := url.Values{}
+	postedData.Add("b", "b")
+
+	form = New(postedData)
+
+	hasVar = form.Has("b")
+	if !hasVar {
+		t.Error("form says it doensn't have b, but it does")
+	}
+}
+
+func TestForm_MinLength(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	isValid := form.MinLength("a", 3)
+	if isValid {
+		t.Error("a doesn't exists, so minvalue should be false")
+	}
+
+	postedData := url.Values{}
+	postedData.Add("b", "bqwe")
+
+	r, _ = http.NewRequest("POST", "/whatever", nil)
+
+	r.PostForm = postedData
+	form = New(r.PostForm)
+
+	isValid = form.MinLength("b", 3)
+	if !isValid {
+		t.Error("b contains more than 3 chars, so should be valid")
+	}
+}
